a4a445633754a452c573b20c73c3c6049a240a87
Kirill Mokevnin <mokevnin@gmail.com>
2016-09-29 15:59:54 +0300

    simplify welcome server

--- src/welcome.js
+++ src/welcome.js
@@ -4,15 +4,5 @@ import http from 'http';
 import { Log } from './definitions';
 
 export default (log: Log) => http.createServer((request, response) => {
-  const url = request.url;
-  const body = [];
-
-  log(`request: ${url}`);
-
-  request
-    .on('data', chunk => body.push(chunk))
-    .on('end', () => {
-      response.write('hello, world!');
-      response.end();
-    });
+  response.end('hello, world!');
 });
